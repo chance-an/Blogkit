e2217c408e312d9f24ebaa3601bbf53a44491149
Changsi An <anchangsi@gmail.com>
2012-02-18 01:17:31 -0500

    remove the code based on extjs

--- admin/js/main.js
+++ admin/js/main.js
@@ -5,33 +5,3 @@
  */
 
 'use strict';
-
-Ext.Loader.setConfig({enabled:true});
-
-Ext.namespace('BKAdmin.layout');
-Ext.Loader.setPath('BKAdmin', './js');
-//Ext.require('Ext.container.Viewport');
-
-Ext.application({
-    name: 'BKAdmin',
-
-    appFolder: 'js',
-
-    controllers : [
-        'Login', 'User'
-    ],
-
-    launch: function() {
-        Blogkit.util.TemplateManager.initialize('template');
-
-        var ready = Ext.create('BKAdmin.view.Viewport');
-        ready.done(Ext.bind(this.goToPage, this, ['Login']));
-    },
-
-    goToPage: function(page){
-        var controller = this.getController(page);
-        controller.start();
-    }
-});
-
-
